99803231133d03e96dd194657be52cb1b1699f5c
MrWasimAbbasi <imwasimabbasi@gmail.com>
2025-07-02 01:16:30 +0500

    fix

--- resources/views/livewire/price-configurator.blade.php
+++ resources/views/livewire/price-configurator.blade.php
@@ -1,17 +1,6 @@
-<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-    {{-- Left: Product Configurator (2/3 width) --}}
-    <div class="md:col-span-2 space-y-6">
-        {{-- Your existing product selection UI goes here --}}
-        @livewire('product-selector')
-        @livewire('user-type-toggle')
-        @foreach ($selectedProducts as $product)
-            @livewire('product-box', ['product' => $product], key($product->id))
-        @endforeach
-        @livewire('final-price-summary', ['products' => $selectedProducts])
-    </div>
-
-    {{-- Right: Explanation Box --}}
-    <div class="space-y-4">
-        @livewire('calculation-breakdown', ['products' => $selectedProducts])
-    </div>
+<div class="max-w-4xl mx-auto space-y-6">
+    <livewire:user-type-toggle/>
+    <livewire:product-selector/>
+    <livewire:product-list/>
+    <livewire:final-price-summary/>
 </div>
